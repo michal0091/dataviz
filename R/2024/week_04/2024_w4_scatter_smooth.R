8201699c4c4283ba8babbb98d99c8f06d0000a4a
Michal Kinel <michal.kinel@gmail.com>
2024-01-28 10:39:55 +0100

    Add base script

---
+++ R/2024/week_04/2024_w4_scatter_smooth.R
@@ -0,0 +1,41 @@
+# Header ------------------------------------------------------------------
+#
+# Author: Michal Kinel
+# Copyright (c) Michal Kinel, 2024
+# Email:  michal.kinel@gmail.com
+# 
+# Date: 2024-01-28
+#
+# Script Name: 2024_w4_scatter_smooth.R
+#
+# Script Description: The ideological gap between men and women in Spain
+#
+#
+# Notes: 
+#
+#
+
+# Set options -------------------------------------------------------------
+cat("Setting options... \n\n", sep = "")
+options(scipen = 999) # turns off scientific notation
+options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
+
+
+# Install packages & load libraries ---------------------------------------
+cat("Install packages & load libraries... \n\n", sep = "")
+packages <- c("tidyverse", "data.table", "zoo", "extrafont") # list of packages to load
+n_packages <- length(packages) # count how many packages are required
+
+new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
+
+# Install missing packages
+if(length(new_pkg)){
+  install.packages(new_pkg)
+}
+
+# Load all requried libraries
+for(n in 1:n_packages){
+  cat("Loading Library #", n, " of ", n_packages, "... Currently Loading: ", packages[n], "\n", sep = "")
+  lib_load <- paste("library(\"",packages[n],"\")", sep = "") # create string of text for loading each library
+  eval(parse(text = lib_load)) # evaluate the string to load the library
+}
