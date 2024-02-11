1cf319f55f5e3a49210a00aed9bf34354ac84adc
Michal Kinel <michal.kinel@gmail.com>
2024-02-11 21:45:19 +0100

    Add base script week 6

---
+++ R/2024/week_06/2024_w06_area_graph.R
@@ -0,0 +1,53 @@
+# Header ------------------------------------------------------------------
+#
+# Author: Michal Kinel
+# Copyright (c) Michal Kinel, 2024
+# Email:  michal.kinel@gmail.com
+# 
+# Date: 2024-02-11
+#
+# Script Name: 2024_w06_area_graph.R
+#
+# Script Description:
+#
+#
+# Notes:
+#
+#
+
+# Set options -------------------------------------------------------------
+cat("Setting options... \n\n", sep = "")
+options(scipen = 999) # turns off scientific notation
+options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
+
+
+# Install packages & load libraries ---------------------------------------
+cat("Install packages & load libraries... \n\n", sep = "")
+packages <- c("tidyverse", "data.table", "zoo", "extrafont") # list of packages to load
+n_packages <- length(packages) # count how many packages are required
+
+new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
+
+# Install missing packages
+if(length(new_pkg)){
+  install.packages(new_pkg)
+}
+
+# Load all requried libraries
+for(n in 1:n_packages){
+  cat("Loading Library #", n, " of ", n_packages, "... Currently Loading: ", packages[n], "\n", sep = "")
+  lib_load <- paste("library(\"",packages[n],"\")", sep = "") # create string of text for loading each library
+  eval(parse(text = lib_load)) # evaluate the string to load the library
+}
+
+
+# Load data ---------------------------------------------------------------
+cat("Load data... \n\n", sep = "")
+
+net_wealth <- fread("R/2024/week_06/median_net_wealth_by_age.csv")
+
+
+
+# Styles ------------------------------------------------------------------
+cat("Setting style... \n\n", sep = "")
+
