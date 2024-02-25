0a4c7a959285d1ddc68b16c01e63a7ab569e789e
Michal Kinel <michal.kinel@gmail.com>
2024-02-25 20:52:30 +0100

    Add base script of week 8

---
+++ R/2024/week_08/2024_w08_test_new_ggplot_3_5.R
@@ -0,0 +1,44 @@
+# Header ------------------------------------------------------------------
+#
+# Author: Michal Kinel
+# Copyright (c) Michal Kinel, 2024
+# Email:  michal.kinel@gmail.com
+# 
+# Date: 2024-02-25
+#
+# Script Name: 2024_w08_test_new_ggplot_3_5.R
+#
+# Script Description: Test new ggplot 3.5.0 version. Gradientes,
+# Ignorign scales, etc.
+#
+#
+# Notes:
+#
+#
+
+# Set options -------------------------------------------------------------
+cat("Setting options... \n\n", sep = "")
+options(scipen = 999) # turns off scientific notation
+options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
+
+
+# Install packages & load libraries ---------------------------------------
+cat("Install packages & load libraries... \n\n", sep = "")
+packages <- c("ggplot2", "data.table", "patchwork", "grid", "extrafont") # list of packages to load
+n_packages <- length(packages) # count how many packages are required
+
+new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
+
+# Install missing packages
+if(length(new_pkg)){
+  install.packages(new_pkg)
+}
+
+# Load all requried libraries
+for(n in 1:n_packages){
+  cat("Loading Library #", n, " of ", n_packages, "... Currently Loading: ", packages[n], "\n", sep = "")
+  lib_load <- paste("library(\"",packages[n],"\")", sep = "") # create string of text for loading each library
+  eval(parse(text = lib_load)) # evaluate the string to load the library
+}
+
+
