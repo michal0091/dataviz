567dc71b53a23a902477f2540f99b25a53b8c065
Michal Kinel <michal.kinel@gmail.com>
2024-03-09 21:47:37 +0100

    Add base script of week 10

---
+++ R/2024/week_10/2024_w10_gender_pay_gap.R
@@ -0,0 +1,51 @@
+# Header ------------------------------------------------------------------
+#
+# Author: Michal Kinel
+# Copyright (c) Michal Kinel, 2024
+# Email:  michal.kinel@gmail.com
+# 
+# Date: 2024-03-09
+#
+# Script Name: 2024_w10_gender_pay_gap.R
+#
+# Script Description: Gender pay gap evolution in EU countries between 2012 and 
+# 2022
+#
+#
+# Notes:
+#
+#
+
+# Set options -------------------------------------------------------------
+cat("Setting options... \n\n", sep = "")
+options(scipen = 999) # turns off scientific notation
+options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
+
+
+# Install packages & load libraries ---------------------------------------
+cat("Install packages & load libraries... \n\n", sep = "")
+packages <- c("tidyverse", "data.table", "zoo") # list of packages to load
+n_packages <- length(packages) # count how many packages are required
+
+new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
+
+# Install missing packages
+if(length(new_pkg)){
+  install.packages(new_pkg)
+}
+
+# Load all requried libraries
+for(n in 1:n_packages){
+  cat("Loading Library #", n, " of ", n_packages, "... Currently Loading: ", packages[n], "\n", sep = "")
+  lib_load <- paste("library(\"",packages[n],"\")", sep = "") # create string of text for loading each library
+  eval(parse(text = lib_load)) # evaluate the string to load the library
+}
+
+
+# Load data ---------------------------------------------------------------
+cat("Load data... \n\n", sep = "")
+
+gender <- fread("R/2024/week_10/gender_pay_gap_2022.csv", dec = ",") 
+
+
+
