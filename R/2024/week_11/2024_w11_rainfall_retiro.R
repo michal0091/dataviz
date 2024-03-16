7f883cc502897e1a844ad639baf202811951a660
Michal Kinel <michal.kinel@gmail.com>
2024-03-16 23:20:34 +0100

    Add plots

--- R/2024/week_11/2024_w11_rainfall_retiro.R
+++ R/2024/week_11/2024_w11_rainfall_retiro.R
@@ -23,7 +23,7 @@ options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
 
 # Install packages & load libraries ---------------------------------------
 cat("Install packages & load libraries... \n\n", sep = "")
-packages <- c("tidyverse", "data.table", "zoo") # list of packages to load
+packages <- c("tidyverse", "data.table", "showtext", "sysfonts") # list of packages to load
 n_packages <- length(packages) # count how many packages are required
 
 new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
@@ -46,18 +46,163 @@ cat("Load data... \n\n", sep = "")
 rainfall <- fread("R/2024/week_11/rainfall_retiro.csv", dec = ",")
 
 rainfall[, year := nafill(year, "locf")]
-rainfall[, month := nafill(month, fill = 0)]
+
+# Subset 1923-2023
+rainfall <- rainfall[year >= 1923]
+
+# Styles ------------------------------------------------------------------
+cat("Styles... \n\n", sep = "")
+
+# Color palette
+color_background <- "#fcfbf9"
+color_font <- "#252b35"
+color_accent_1 <- "#79253B"
+color_accent_2 <- "#3B7925"
+color_gradient_low <- "#253b79"
+color_gradient_high <- "#94cfcb"
+
+# Fonts
+font_add_google(name = "Lato")
+font_base <- "Lato"
+showtext_auto()
 
 
 # Plot --------------------------------------------------------------------
 cat("Plot... \n\n", sep = "")
 
-rainfall[, ggplot(.SD, aes(x = reorder(factor(month), -month), y = mm, color = year)) +
-           geom_jitter(position = position_jitter(seed = 1991, width = 0.2), size = 2, alpha = 0.25) +
-           stat_summary(data = .SD[year <= 2000], fun = mean, geom = "point", size = 5) +
-           stat_summary(data = .SD[year > 2000], fun = mean, geom = "point", size = 5) +
-           coord_flip() +
-           scale_y_continuous(limits = c(0, 220), expand = c(0.08, 0))]
 
+plot_1 <- rainfall[, ggplot(.SD, aes(
+  x = reorder(factor(month), -month),
+  y = mm,
+  color = year
+)) +
+  geom_jitter(
+    position = position_jitter(seed = 1991, width = 0.2),
+    size = 3,
+    alpha = .5
+  ) +
+  stat_summary(
+    data = .SD[year <= 1955],
+    fun = mean,
+    geom = "point",
+    aes(shape = "≤ 1955"),
+    size = 5,
+    color = color_accent_2
+  ) +
+  stat_summary(
+    data = .SD[year > 1990],
+    fun = mean,
+    geom = "point",
+    aes(shape = "> 1990"),
+    size = 5,
+    color = color_accent_1
+  ) +
+  scale_shape_manual("Mean", values =  c("≤ 1955" = 15, "> 1990" = 17)) +
+  coord_flip() +
+  scale_y_continuous(limits = c(0, 200), expand = c(0.08, 0)) +
+  scale_colour_steps(low = color_gradient_low, high = color_gradient_high) +
+  labs(
+    title = "Rainfall in Retiro, Madrid",
+    subtitle = "Monthly rainfall in Retiro, Madrid, 1923-2023",
+    caption = "Source: AEMET",
+    x = "Month",
+    y = "Rainfall (mm)",
+    color = NULL
+  ) +
+  theme_void() +
+  theme(
+    text = element_text(
+      family = font_base,
+      color = color_font,
+      size = 16
+    ),
+    plot.margin = margin(1, 1, 1, 1, "cm"),
+    plot.background = element_rect(fill = color_background, color  = NA),
+    legend.position = "bottom",
+    legend.key.width = unit(1.4, "cm"),
+    legend.key.height = unit(0.35, "cm"),
+    axis.text = element_text(
+      color = color_font,
+      family = font_base,
+      size = 16
+    ),
+    axis.title = element_text(
+      color = color_font,
+      family = font_base,
+      size = 16
+    ),
+    axis.title.y = element_text(
+      color = color_font,
+      family = font_base,
+      size = 16,
+      angle = 90
+    ),
+    plot.title = element_text(
+      hjust = 0,
+      vjust = 2,
+      color = color_font,
+      family = font_base,
+      size = 26,
+      face = "bold"
+    )
+  )]
+
+sum_year <- rainfall[, .(sum_year = sum(mm, na.rm = TRUE)), year]
+sum_year[, rolling_mean_10y := frollmean(sum_year, 10, align = "right", fill = NA)]
+sum_year[, rolling_sd_10y := frollapply(sum_year, 10, sd, align = "right", fill = NA)]
+
+plot_2 <- sum_year[, ggplot(.SD, aes(year, sum_year)) +
+                     geom_line(color = color_gradient_low) +
+                     geom_line(aes(year, rolling_mean_10y), color = color_gradient_high) +
+                     geom_ribbon(aes(y = sum_year, 
+                                     ymin = rolling_mean_10y - rolling_sd_10y, 
+                                     ymax = rolling_mean_10y + rolling_sd_10y), 
+                                 fill = color_gradient_high, 
+                                 alpha = .2) +
+                     scale_x_continuous(breaks = seq(1923, 2023, 20), 
+                                        limits = c(1923, 2023), 
+                                        expand = c(.02, .02)) +
+                     scale_y_continuous(limits = c(0, 800),
+                                        breaks = seq(0, 800, 100)) +
+                     theme_void() +
+                     theme(
+                       text = element_text(
+                         family = font_base,
+                         color = color_font,
+                         size = 16
+                       ),
+                       plot.margin = margin(1, 1, 1, 1, "cm"),
+                       plot.background = element_rect(fill = color_background, color  = NA),
+                       axis.text = element_text(
+                         color = color_font,
+                         family = font_base,
+                         size = 16
+                       ),
+                       axis.title = element_text(
+                         color = color_font,
+                         family = font_base,
+                         size = 16
+                       ),
+                       axis.title.y = element_text(
+                         color = color_font,
+                         family = font_base,
+                         size = 16,
+                         angle = 90
+                       ),
+                       plot.title = element_text(
+                         hjust = 0,
+                         vjust = 2,
+                         color = color_font,
+                         family = font_base,
+                         size = 26,
+                         face = "bold"
+                       ),
+                       panel.grid.major = element_line(
+                         colour = color_font,
+                         linetype = "dotted",
+                         linewidth = .3
+                       )
+                     )]
+plot_2
 
 
