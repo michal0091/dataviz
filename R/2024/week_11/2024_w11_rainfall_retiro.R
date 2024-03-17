35133c4ac02a4cd94340a4edff42e7e6e2373378
Michal Kinel <michal.kinel@gmail.com>
2024-03-17 12:34:56 +0100

    Add social and save

--- R/2024/week_11/2024_w11_rainfall_retiro.R
+++ R/2024/week_11/2024_w11_rainfall_retiro.R
@@ -23,7 +23,8 @@ options(encoding = "UTF-8") # sets string encoding to UTF-8 instead of ANSI
 
 # Install packages & load libraries ---------------------------------------
 cat("Install packages & load libraries... \n\n", sep = "")
-packages <- c("tidyverse", "data.table", "showtext", "sysfonts") # list of packages to load
+packages <- c("tidyverse", "data.table", "showtext", "sysfonts", "patchwork",
+              "emojifont") # list of packages to load
 n_packages <- length(packages) # count how many packages are required
 
 new_pkg <- packages[!(packages %in% installed.packages())] # determine which packages aren't installed
@@ -70,7 +71,6 @@ showtext_auto()
 # Plot --------------------------------------------------------------------
 cat("Plot... \n\n", sep = "")
 
-
 plot_1 <- rainfall[, ggplot(.SD, aes(
   x = reorder(factor(month), -month),
   y = mm,
@@ -78,15 +78,16 @@ plot_1 <- rainfall[, ggplot(.SD, aes(
 )) +
   geom_jitter(
     position = position_jitter(seed = 1991, width = 0.2),
-    size = 3,
-    alpha = .5
+    size = 2,
+    alpha = .35, 
+    stroke = 0
   ) +
   stat_summary(
     data = .SD[year <= 1955],
     fun = mean,
     geom = "point",
     aes(shape = "≤ 1955"),
-    size = 5,
+    size = 2,
     color = color_accent_2
   ) +
   stat_summary(
@@ -94,17 +95,15 @@ plot_1 <- rainfall[, ggplot(.SD, aes(
     fun = mean,
     geom = "point",
     aes(shape = "> 1990"),
-    size = 5,
+    size = 3,
     color = color_accent_1
   ) +
-  scale_shape_manual("Mean", values =  c("≤ 1955" = 15, "> 1990" = 17)) +
+  scale_shape_manual("Mean", values =  c("≤ 1955" = 5, "> 1990" = 18)) +
   coord_flip() +
   scale_y_continuous(limits = c(0, 200), expand = c(0.08, 0)) +
   scale_colour_steps(low = color_gradient_low, high = color_gradient_high) +
   labs(
-    title = "Rainfall in Retiro, Madrid",
-    subtitle = "Monthly rainfall in Retiro, Madrid, 1923-2023",
-    caption = "Source: AEMET",
+    title = "Monthly rainfall",
     x = "Month",
     y = "Rainfall (mm)",
     color = NULL
@@ -114,27 +113,27 @@ plot_1 <- rainfall[, ggplot(.SD, aes(
     text = element_text(
       family = font_base,
       color = color_font,
-      size = 16
+      size = 14
     ),
-    plot.margin = margin(1, 1, 1, 1, "cm"),
+    plot.margin = margin(1, 1, 0, 1, "cm"),
     plot.background = element_rect(fill = color_background, color  = NA),
     legend.position = "bottom",
-    legend.key.width = unit(1.4, "cm"),
-    legend.key.height = unit(0.35, "cm"),
+    legend.key.width = unit(.6, "cm"),
+    legend.key.height = unit(.15, "cm"),
     axis.text = element_text(
       color = color_font,
       family = font_base,
-      size = 16
+      size = 18
     ),
     axis.title = element_text(
       color = color_font,
       family = font_base,
-      size = 16
+      size = 18
     ),
     axis.title.y = element_text(
       color = color_font,
       family = font_base,
-      size = 16,
+      size = 18,
       angle = 90
     ),
     plot.title = element_text(
@@ -142,7 +141,7 @@ plot_1 <- rainfall[, ggplot(.SD, aes(
       vjust = 2,
       color = color_font,
       family = font_base,
-      size = 26,
+      size = 22,
       face = "bold"
     )
   )]
@@ -151,72 +150,157 @@ sum_year <- rainfall[, .(sum_year = sum(mm, na.rm = TRUE)), year]
 sum_year[, rolling_mean_10y := frollmean(sum_year, 10, align = "right", fill = NA)]
 sum_year[, rolling_sd_10y := frollapply(sum_year, 10, sd, align = "right", fill = NA)]
 
-plot_2 <- sum_year[, ggplot(.SD, aes(year, sum_year, color = "Yearly rainfall")) +
-                     geom_line() +
-                     geom_line(aes(year, rolling_mean_10y, color = "10y roll mean")) +
-                     geom_ribbon(aes(y = sum_year, 
-                                     ymin = rolling_mean_10y - rolling_sd_10y, 
-                                     ymax = rolling_mean_10y + rolling_sd_10y,
-                                     fill = "± sd"),
-                                 alpha = .2, 
-                                 color = NA) +
-                     scale_x_continuous(breaks = seq(1923, 2023, 20), 
-                                        limits = c(1923, 2023), 
-                                        expand = c(.02, .02)) +
-                     scale_y_continuous(limits = c(0, 800),
-                                        breaks = seq(0, 800, 100)) +
-                     scale_color_manual(values = c("Yearly rainfall" = color_gradient_low,
-                                                   "10y roll mean" = color_gradient_high)) +
-                     scale_fill_manual(values = c("± sd" = color_gradient_high)) +
-                     labs(
-                       title = "Rainfall in Retiro, Madrid",
-                       subtitle = "Yearly rainfall in Retiro, Madrid, 1923-2023",
-                       caption = "Source: AEMET",
-                       x = "Year",
-                       y = "Rainfall (mm)",
-                       color = NULL,
-                       fill = NULL
-                     ) +
-                     theme_void() +
-                     theme(
-                       text = element_text(
-                         family = font_base,
-                         color = color_font,
-                         size = 16
-                       ),
-                       plot.margin = margin(1, 1, 1, 1, "cm"),
-                       plot.background = element_rect(fill = color_background, color  = NA),
-                       legend.position = "bottom",
-                       axis.text = element_text(
-                         color = color_font,
-                         family = font_base,
-                         size = 16
-                       ),
-                       axis.title = element_text(
-                         color = color_font,
-                         family = font_base,
-                         size = 16
-                       ),
-                       axis.title.y = element_text(
-                         color = color_font,
-                         family = font_base,
-                         size = 16,
-                         angle = 90
-                       ),
-                       plot.title = element_text(
-                         hjust = 0,
-                         vjust = 2,
-                         color = color_font,
-                         family = font_base,
-                         size = 26,
-                         face = "bold"
-                       ),
-                       panel.grid.major = element_line(
-                         colour = color_font,
-                         linetype = "dotted",
-                         linewidth = .3
-                       )
-                     )]
-plot_2
+plot_2 <-
+  sum_year[, ggplot(.SD, aes(year, sum_year, color = "Yearly rainfall")) +
+             geom_line() +
+             geom_line(aes(year, rolling_mean_10y, color = "10y roll mean")) +
+             geom_ribbon(
+               aes(
+                 y = sum_year,
+                 ymin = rolling_mean_10y - rolling_sd_10y,
+                 ymax = rolling_mean_10y + rolling_sd_10y,
+                 fill = "± sd"
+               ),
+               alpha = .2,
+               color = NA
+             ) +
+             scale_x_continuous(
+               breaks = seq(1923, 2023, 20),
+               limits = c(1923, 2023),
+               expand = c(.02, .02)
+             ) +
+             scale_y_continuous(limits = c(0, 800),
+                                breaks = seq(0, 800, 100)) +
+             scale_color_manual(values = c("Yearly rainfall" = color_gradient_low,
+                                           "10y roll mean" = color_gradient_high)) +
+             scale_fill_manual(values = c("± sd" = color_gradient_high)) +
+             labs(
+               title = "Yearly rainfall",
+               x = "Year",
+               y = "Rainfall (mm)",
+               color = NULL,
+               fill = NULL
+             ) +
+             theme_void() +
+             theme(
+               text = element_text(
+                 family = font_base,
+                 color = color_font,
+                 size = 16
+               ),
+               plot.margin = margin(1, 1, 0, 1, "cm"),
+               plot.background = element_rect(fill = color_background, color  = NA),
+               legend.position = "bottom",
+               axis.text = element_text(
+                 color = color_font,
+                 family = font_base,
+                 size = 18
+               ),
+               axis.title = element_text(
+                 color = color_font,
+                 family = font_base,
+                 size = 18
+               ),
+               axis.title.y = element_text(
+                 color = color_font,
+                 family = font_base,
+                 size = 18,
+                 angle = 90
+               ),
+               plot.title = element_text(
+                 hjust = 0,
+                 vjust = 2,
+                 color = color_font,
+                 family = font_base,
+                 size = 22,
+                 face = "bold"
+               ),
+               panel.grid.major = element_line(
+                 colour = color_font,
+                 linetype = "dotted",
+                 linewidth = .3
+               )
+             )]
 
+
+# Caption -----------------------------------------------------------------
+cat("Caption... \n\n", sep = "")
+source_text <- "AEMET"
+highlight_color <- color_gradient_low
+text_color <- color_font
+github_icon <- '&#xf113'
+github_username <- "michal0091"
+twitter_icon <- "&#xf081"
+twitter_username <- "nico_kinel"
+linkedin_icon <- "&#xf08c"
+linkedin_username <- "michal-kinel"
+mastodon_icon <- "&#xf4f6"
+mastodon_username <- "miki_peltzer"
+mastodon_server <- "techhub.social"
+social_caption <- glue::glue(
+  "<span style='color: {text_color}'>Source: {source_text}</span><br>
+  <span style='font-family:\"fa-brands\";
+                color: {highlight_color}'>{github_icon};</span>
+  <span style='color: {text_color}'>{github_username}</span>
+  <span style='font-family:\"fa-brands\";
+                color: {highlight_color}'>{twitter_icon};</span>
+  <span style='color: {text_color}'>{twitter_username}</span>
+  <span style='font-family:\"fa-brands\";
+                color: {highlight_color}'>{linkedin_icon};</span>
+  <span style='color: {text_color}'>{linkedin_username}</span>
+  <span style='font-family:\"fa-brands\";
+                color: {highlight_color}'>{mastodon_icon};</span>
+  <span style='color: {text_color}'>{mastodon_username}@<span><span style='color: {text_color}'>{mastodon_server}</span>"
+)
+
+
+# Combine plots -----------------------------------------------------------
+cat("Combine plots... \n\n", sep = "")
+
+combined_plot <-
+  plot_1 + plot_2 +
+  plot_layout(widths = c(1, 2)) +
+  plot_annotation(
+    title = "Rainfall in Retiro, Madrid",
+    subtitle = "Monthly and yearly rainfall between 1923 and 2023",
+    caption = social_caption,
+    theme = theme(
+      plot.background = element_rect(fill = color_background, color = NA),
+      plot.title = element_text(
+        hjust = 0,
+        vjust = 0,
+        color = color_font,
+        family = font_base,
+        size = 26,
+        face = "bold"
+      ),
+      plot.subtitle = element_text(
+        hjust = 0,
+        vjust = 1,
+        color = color_font,
+        family = font_base,
+        size = 22
+      ),
+      plot.caption = element_markdown(
+        hjust = 0,
+        halign = 0,
+        family = font_base,
+        size = 14,
+        color = color_font
+      )
+    )
+  )
+
+# Save plot ---------------------------------------------------------------
+cat("Save plot... \n\n", sep = "")
+
+ggsave(
+  filename = "rainfall_retiro.png",
+  path = normalizePath("R/2024/week_11/"),
+  plot = combined_plot,
+  device = "png",
+  units = "cm",
+  width = 21,
+  height = 13
+)
 
