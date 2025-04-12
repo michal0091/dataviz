0e3142f5e48c1b20bae878bf31c560de9f5f491e
Michal Kinel <michal.kinel@gmail.com>
2025-04-12 11:02:44 +0200

    Add font

--- R/30DayChartChallenge2025/utils.R
+++ R/30DayChartChallenge2025/utils.R
@@ -102,6 +102,7 @@ setup_fonts <- function(fa_brands_path = "fonts/fa-brands-400.ttf") {
   font_add_google("Baumans", "Baumans")
   font_add_google("Roboto Mono", "Roboto Mono") 
   font_add_google("Exo 2", "Exo 2") 
+  font_add_google("Cabin", "Cabin")
 
   # Cargar fuente de iconos local (Font Awesome Brands)
   # ¡¡VERIFICA QUE LA RUTA 'fa_brands_path' ES CORRECTA!!
